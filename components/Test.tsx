a5b7b132e8a0eb645e05b89e14ff5b06bb0ab784
denisrudov <denis.rudov@gmail.com>
2022-04-09 01:21:28 +0200

    init

---
+++ components/Test.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  background-color: #0070f3;
+`
+
+
+const Test = () => {
+  return (
+    <Wrapper>
+        111
+        222
+    </Wrapper>
+  )
+}
+
+export default Test
